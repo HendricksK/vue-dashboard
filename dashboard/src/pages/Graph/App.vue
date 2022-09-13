278555d0e3ac858ddb761a89844fb804c8558e50
hendricksakurvin <88704296+hendricksakurvin@users.noreply.github.com>
2022-09-13 03:16:35 +0200

    graph is now dynamic

--- dashboard/src/pages/Graph/App.vue
+++ dashboard/src/pages/Graph/App.vue
@@ -1,6 +1,17 @@
 <template>
-  <BarGraph msg="Welcome to Your Vue.js App"/>
-  <LineGraph msg="Welcome to Your Vue.js App"/>
+  <BarGraphTest msg="Welcome to Your Vue.js App"/>
+  <LineGraphTest msg="Welcome to Your Vue.js App"/>
+  <Bar
+    :chart-options="chartOptions"
+    :chart-data="chartData"
+    :chart-id="chartId"
+    :dataset-id-key="datasetIdKey"
+    :plugins="plugins"
+    :css-classes="cssClasses"
+    :styles="styles"
+    :width="width"
+    :height="height"
+  />
 
   <a href="https://ghibliapi.herokuapp.com/#section/Getting-Started">https://ghibliapi.herokuapp.com/#section/Getting-Started</a>
   <div v-for="film in films" :key="film.id">
@@ -13,32 +24,97 @@
   </div>
 </template>
 
+
+
 <script>
-import BarGraph from '../../components/graphs/test/Bar.vue'
-import LineGraph from '../../components/graphs/test/Line.vue'
+import BarGraphTest from '../../components/graphs/test/Bar.vue'
+import LineGraphTest from '../../components/graphs/test/Line.vue'
 import axios from 'axios'
+
+import { Bar } from 'vue-chartjs'
+import { Chart as ChartJS, Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale } from 'chart.js'
+
+ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale)
 
 export default {
   name: 'App',
   components: {
-    BarGraph,
-    LineGraph,
+    BarGraphTest,
+    LineGraphTest,
+    Bar
+  },
+  props: {
+    chartId: {
+      type: String,
+      default: 'bar-chart'
+    },
+    datasetIdKey: {
+      type: String,
+      default: 'label'
+    },
+    width: {
+      type: Number,
+      default: 400
+    },
+    height: {
+      type: Number,
+      default: 400
+    },
+    cssClasses: {
+      default: '',
+      type: String
+    },
+    styles: {
+      type: Object,
+      default: () => {}
+    },
+    plugins: {
+      type: Object,
+      default: () => {}
+    }
   },
   data() {
     return {
-      films: []
+      films: [],
+      chartData: {
+        labels: [],
+        datasets: [],
+      },
+      chartOptions: {
+        responsive: true,
+        maintainAspectRatio: false,
+        // https://stackoverflow.com/questions/70509197/bar-chart-js-legend-showing-undefined
+      }
     }
   },
   mounted() {
+    // let datasets = []
     // https://www.koderhq.com/tutorial/vue/http-axios/
     axios.get('https://ghibliapi.herokuapp.com/films')
     .then((response) => {
-      console.log(response)
       this.films = response.data
     })
+
+    let endpoints = [
+      'https://openlibrary.org/subjects/horror.json?published_in=1800-2000',
+      'https://openlibrary.org/subjects/comedy.json?published_in=1800-2000',
+      'https://openlibrary.org/subjects/travel.json?published_in=1800-2000'
+    ];
+
+    axios.all(endpoints.map((endpoint) => axios.get(endpoint))).then(
+      (data) => {
+        let datasets = []
+        for (let x = 0; x < data.length; x++) {
+          this.chartData.labels.push(data[x].data.name)
+          datasets.push(data[x].data.work_count)
+        }
+        this.chartData.datasets.push({label: 'No of books published between 1800 - 2000', data: datasets})
+      },
+    );
+
+
   }
 }
-
 </script>
 
 <style>
